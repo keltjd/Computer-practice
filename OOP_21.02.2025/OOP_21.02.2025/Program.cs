11dbf0fe4b97803bfa450f87e7812e963248aa3f
gdd.24 <gdd.24@WS41609>
2025-02-21 11:48:02 +0300

    занятие по ооп за 21.02

---
+++ OOP_21.02.2025/OOP_21.02.2025/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace programm
+{
+    public class QuadraticAnswer
+    {
+        private double X1, X2;
+        public QuadraticAnswer(double X1, double X2)
+        {
+            this.X1 = X1;
+            this.X2 = X2;
+        }
+    }
+    public class Quadratic
+    {
+
+        private double a, b, c;
+
+        public double A
+        {
+            get { return this.a; }
+            set { this.a = value; }
+        }
+
+        public double B
+        {
+            get { return this.a; }
+            set { this.b = value; }
+        }
